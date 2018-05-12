e81dd490dc8a6e53dd929aaa893de8ab0f5aea2d
Joydeep Bhattacharya <joydeep1982@gmail.com>
2018-05-12 09:53:55 +0530

    Update PrimeNumber.java

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -6,18 +6,20 @@ public class PrimeNumber {
 
 	public static boolean isPrimeImperative(int number) {
 		
-		boolean divisible = false;
-		for (int i = 2; i < number; i++) {
-			if (number % 2 == 0) {
-				divisible = true;
-				break;
-			}
-		}		
-		return number > 1 && !divisible;
+        boolean divisible = false;
+        for (int i = 2; i < number; i++) {
+                if (number % 2 == 0) {
+                        divisible = true;
+                        break;
+                }
+        }		
+        return number > 1 && !divisible;
 	}
   
     public static boolean isPrimeFunctional(int number) {
-		return number > 1 && IntStream.range(2, number).noneMatch(i -> number % i == 0);
+		return number > 1 && 
+            IntStream.range(2, number)
+                .noneMatch(i -> number % i == 0);
     }
     
 }
