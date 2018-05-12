173153433819e70bb1958cc636034199a8fbb498
Joydeep Bhattacharya <joydeep1982@gmail.com>
2018-05-12 09:45:09 +0530

    Create PrimeNumber.java

---
+++ PrimeNumber.java
@@ -0,0 +1,23 @@
+package functional.programming;
+
+import java.util.stream.IntStream;
+
+public class PrimeNumber {
+
+	public static boolean isPrimeImperative(int number) {
+		
+		boolean divisible = false;
+		for (int i = 2; i < number; i++) {
+			if (number % 2 == 0) {
+				divisible = true;
+				break;
+			}
+		}		
+		return number > 1 && !divisible;
+	}
+  
+    public static boolean isPrimeFunctional(int number) {
+		return number > 1 && IntStream.range(2, number).noneMatch(i -> number % i == 0);
+    }
+    
+}
