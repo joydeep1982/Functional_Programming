85a94c62cf7962102072c535d4440b1d708d8b04
Joydeep Bhattacharya <joydeep1982@gmail.com>
2018-05-12 09:50:24 +0530

    Create Palindrome.java

---
+++ Palindrome.java
@@ -0,0 +1,29 @@
+package functional.programming;
+
+import java.util.stream.IntStream;
+
+public class Palindrome {
+  
+  public boolean isPalindromeFunctional(int number) {
+		
+			return number == IntStream.iterate(number, i -> i / 10)
+				.map(num -> num % 10)
+				.limit(String.valueOf(number).length())
+				.reduce(0, (carry, value) -> carry * 10 + value);
+	}
+	
+	public boolean isPalindromeImperative(int number) {
+		int n = number;
+		
+		int digit;
+		int reverse = 0;
+		
+		do {
+			digit = n % 10;
+			reverse = reverse * 10 + digit;
+			n = n / 10;
+		} while (n > 0);
+		
+		return reverse == number;
+	}
+}
